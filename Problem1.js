2d2e093780a5d051c44c8313a5c7bc678b5221c7
Arun kumar <79135781+arunravi03@users.noreply.github.com>
2022-02-09 11:48:07 +0530

    Update Problem1.js

--- Problem1.js
+++ Problem1.js
@@ -1,14 +1,3 @@
-//1. Write a function to convert a input to a number
-
-/**
- * Algorithm
- *
- * Step1 - Create a function with an input
- * Step2 - Then convert a input to number
- * Step3 - Check whether the input is falsey value (0, null, undefined, false)
- * Step3.1- If the input is falsey and NaN return 0
- */
-
 function myFunction(input) {
   const parsed = parseInt(input);
   if (isNaN(parsed) || parsed === false) {
